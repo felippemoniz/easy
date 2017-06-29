068ec24179db80f9e9988136e5e2d484eb1d0366
MEC\felippemoniz <felippe.moniz@gmail.com>
2017-06-29 13:38:37 -0300

    teste

--- app/services/config.js
+++ app/services/config.js
@@ -1,3 +1,3 @@
 "use strict";
-exports.SERVER_URL = "http://easymovie.kinghost.net:21293/";
-//exports.SERVER_URL = "http://localhost:8080";
+//exports.SERVER_URL = "http://easymovie.kinghost.net:21293/";
+exports.SERVER_URL = "http://localhost:8080";
